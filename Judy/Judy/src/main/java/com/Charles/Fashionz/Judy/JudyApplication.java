a21f96bdf5ec8e5ab5fd86076fafe516217daae9
pramodrajarapu <pramodrajarapu@gmail.com>
2023-08-31 21:20:48 +0530

    annotations are written and the methord is written inside the class

---
+++ Judy/Judy/src/main/java/com/Charles/Fashionz/Judy/JudyApplication.java
@@ -0,0 +1,13 @@
+package com.Charles.Fashionz.Judy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JudyApplication.class, args);
+	}
+
+}
---
+++ Judy/Judy/src/test/java/com/Charles/Fashionz/Judy/JudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Charles.Fashionz.Judy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
